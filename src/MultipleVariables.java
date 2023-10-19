fe211c18e1ac79b93bdab5d69a8b049b05c4861d
salah <warapM8@gmail.com>
2023-10-19 09:10:07 +0100

    created new java project for data type

---
+++ src/MultipleVariables.java
@@ -0,0 +1,21 @@
+/**
+ * @author $(USER)
+ **/
+public class MultipleVariables {
+    void sendFence(){
+        String s1,s2;
+
+        //String s3 = "yes"; s4 ="no";
+    }
+
+    void paintFence(){
+        int i1,i2,i3 =0;
+    }
+
+    void legalDeclaration(){
+
+        boolean b1,b2;
+        String s1 = "1",s2;
+
+    }
+}
---
+++ src/Primitive.java
@@ -0,0 +1,38 @@
+/**
+ * @author $(USER)
+ **/
+public class Primitive {
+    long primitive(){
+        boolean bo = true;
+
+        byte b = 1;
+        b='a';
+
+        short s = 1;
+        s='z';
+
+        int i = 1;
+        i='t';
+
+        long lo = 1L;
+        lo='g';
+
+        float f = 3.14f;
+        f='m';
+
+        double d = 3.14;
+        d='p';
+
+        char c = 'a';
+        c= 1;
+
+        return 1L;
+    }
+
+    public long wrapper(){
+        Boolean bo = false;
+
+        boolean bo = Boolean.valueOf(true);
+        return 1L;
+    }
+}
